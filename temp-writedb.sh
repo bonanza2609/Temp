6e675c5deabf29736e10ef76435a5cc2e4b33dd5
bonanza2609 <48529059+bonanza2609@users.noreply.github.com>
2021-08-15 21:53:22 +0200

    Set gitignore for config fiels

---
+++ temp-writedb.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#
+# add line to crontab:
+# a) use private cron: crontab -e
+# b) use system  cron: sudo nano /etc/crontab
+# */15 * * * * /home/pi/temp/temp-writedb.sh
+# this will read the sensor every 15 minutes
+
+cd /home/pi/data/temp/
+
+## config 1 read (default)
+# read sensors and write into database
+/usr/bin/python temp.py
+
+# read sensors and write into backup-database
+/usr/bin/python temp.py -b
+
+## config 2 read
+# read sensors and write into database
+/usr/bin/python temp.py --conf temp-config-2.txt
+
+# read sensors and write into backup-database
+/usr/bin/python temp.py -b --conf temp-config-2.txt
+
+## config 3 read
+# read sensors and write into database
+/usr/bin/python temp.py --conf temp-config-3.txt
+
+# read sensors and write into backup-database
+/usr/bin/python temp.py -b --conf temp-config-3.txt
+
+## html config 1 (default)
+# create html page with last dataset
+/usr/bin/python temp.py -b -w
+
+## html config 2
+# create html page with last dataset
+/usr/bin/python temp.py -b -w --conf temp-config-2.txt
+
+## html config 3
+# create html page with last dataset
+/usr/bin/python temp.py -b -w --conf temp-config-3.txt
+
+## multii html
+/usr/bin/python temp.py -b -wm temp-config-2.txt,temp-config-anzeige.txt,temp-config-3-anzeige.txt
+
+## copy temp*.html fils
+sudo cp -r www/temp* /var/www/temp/
+
+echo ""
+echo "fiel(s) copyed"
+echo ""
