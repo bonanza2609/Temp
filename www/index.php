8dbf9fdcdff88ae154149e2fa6f4f78cb9f83230
bonanza2609 <48529059+bonanza2609@users.noreply.github.com>
2021-09-07 19:59:17 +0200

    Delete or move files in the web folder

--- www/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-header('Location: /temp/index.html');
-?>
--- www/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-print '<html><body><h1>PHP works also!</h1>
-<p>This is the default PHP web page for this server.</p>
-';
-
-phpinfo();
-
-print '</body></html>';
-?>
